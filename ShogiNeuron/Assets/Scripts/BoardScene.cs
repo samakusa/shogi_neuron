7fa9b2f341c162b1e1e9bebe1a2daef09a7346fc
samakusa <sirouamakusa0412@gmail.com>
2020-12-29 14:54:31 +0900

    Create board screen
    implemented the process of arranging pieces in Start () as a trial.

---
+++ ShogiNeuron/Assets/Scripts/BoardScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardScene : MonoBehaviour {
+    public GameObject piecePrefab;
+    public GameObject onBoard;
+
+    // Start is called before the first frame update
+    void Start() {
+        GameObject piece = (GameObject)Instantiate(piecePrefab);
+        piece.transform.SetParent(this.onBoard.transform, false);
+        piece.transform.localPosition = new Vector3(
+            0f,
+            0f,
+            0f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
