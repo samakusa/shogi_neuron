41c2d992ffb08dbfa0ec402a98af22b61dfb54c4
samakusa <sirouamakusa0412@gmail.com>
2020-12-30 14:19:40 +0900

    Fixed detailed processing of piece drawing processing

--- ShogiNeuron/Assets/Scripts/BoardScene.cs
+++ ShogiNeuron/Assets/Scripts/BoardScene.cs
@@ -6,23 +6,8 @@ public class BoardScene : MonoBehaviour {
     public GameObject onBoard;
     public GameObject piecePrefab;
 
-    private PieceRenderer PieceRenderer;
+    private PieceRenderer PieceRenderer = new PieceRenderer();
     private string begin_board_sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b -";
-
-    private const float M = 0.3525f;
-    private const int COL = 0;  // index
-    private const int ROW = 1;  // index
-    private float[,] p = {
-        {-4*M,-4*M},{-3*M,-4*M},{-2*M,-4*M},{-1*M,-4*M},{0*M,-4*M},{1*M,-4*M},{2*M,-4*M},{3*M,-4*M},{4*M,-4*M},
-        {-4*M,-3*M},{-3*M,-3*M},{-2*M,-3*M},{-1*M,-3*M},{0*M,-3*M},{1*M,-3*M},{2*M,-3*M},{3*M,-3*M},{4*M,-3*M},
-        {-4*M,-2*M},{-3*M,-2*M},{-2*M,-2*M},{-1*M,-2*M},{0*M,-2*M},{1*M,-2*M},{2*M,-2*M},{3*M,-2*M},{4*M,-2*M},
-        {-4*M,-1*M},{-3*M,-1*M},{-2*M,-1*M},{-1*M,-1*M},{0*M,-1*M},{1*M,-1*M},{2*M,-1*M},{3*M,-1*M},{4*M,-1*M},
-        {-4*M, 0*M},{-3*M, 0*M},{-2*M, 0*M},{-1*M, 0*M},{0*M, 0*M},{1*M, 0*M},{2*M, 0*M},{3*M, 0*M},{4*M, 0*M},
-        {-4*M, 1*M},{-3*M, 1*M},{-2*M, 1*M},{-1*M, 1*M},{0*M, 1*M},{1*M, 1*M},{2*M, 1*M},{3*M, 1*M},{4*M, 1*M},
-        {-4*M, 2*M},{-3*M, 2*M},{-2*M, 2*M},{-1*M, 2*M},{0*M, 2*M},{1*M, 2*M},{2*M, 2*M},{3*M, 2*M},{4*M, 2*M},
-        {-4*M, 3*M},{-3*M, 3*M},{-2*M, 3*M},{-1*M, 3*M},{0*M, 3*M},{1*M, 3*M},{2*M, 3*M},{3*M, 3*M},{4*M, 3*M},
-        {-4*M, 4*M},{-3*M, 4*M},{-2*M, 4*M},{-1*M, 4*M},{0*M, 4*M},{1*M, 4*M},{2*M, 4*M},{3*M, 4*M},{4*M, 4*M},
-    };
 
     private enum piece_types {
         NONE, PORN, LANCE, KNIGHT, SILVER, BISHOP, ROOK, GOLD, KING,
@@ -31,8 +16,28 @@ public class BoardScene : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        this.PieceRenderer = new PieceRenderer();
-        this.PieceRenderer.Render(this.onBoard, this.piecePrefab, 0, 0, (int)piece_types.LANCE);
+        GameObject piece1 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece1, 9, 9, (int)piece_types.LANCE);
+        GameObject piece2 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece2, 8, 9, (int)piece_types.KNIGHT);
+        GameObject piece3 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece3, 7, 9, (int)piece_types.SILVER);
+        GameObject piece4 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece4, 6, 9, (int)piece_types.GOLD);
+        GameObject piece5 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece5, 5, 9, (int)piece_types.KING);
+        GameObject piece6 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece6, 4, 9, (int)piece_types.GOLD);
+        GameObject piece7 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece7, 3, 9, (int)piece_types.SILVER);
+        GameObject piece8 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece8, 2, 9, (int)piece_types.KNIGHT);
+        GameObject piece9 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece9, 1, 9, (int)piece_types.LANCE);
+        GameObject piece10 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece10, 8, 8, (int)piece_types.BISHOP);
+        GameObject piece11 = (GameObject)Instantiate(this.piecePrefab);
+        this.PieceRenderer.Render(this.onBoard, piece11, 2, 8, (int)piece_types.ROOK);
     }
 
     // Update is called once per frame
--- ShogiNeuron/Assets/Scripts/PieceRenderer.cs
+++ ShogiNeuron/Assets/Scripts/PieceRenderer.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PieceRenderer : MonoBehaviour
-{
+public class PieceRenderer {
     private const string PIECE_IMG_DIR = "Images/";
     private string[] piece_names = {
         "",
@@ -13,23 +10,43 @@ public class PieceRenderer : MonoBehaviour
         "silver",
         "bishop",
         "rook",
+        "gold",
+        "king",
         "porn_promoted",
         "lance_promoted",
         "knight_promoted",
         "silver_promoted",
         "bishop_promoted",
         "rook_promoted",
-        "gold",
-        "king",
     };
 
-    public void Render(GameObject board, GameObject basePiece, int col, int row, int type) {
-        GameObject piece = (GameObject)Instantiate(basePiece);
+    private const float M = 0.39f;
+    private const int COL = 0;  // index
+    private const int ROW = 1;  // index
+    private float[,] POSITIONS = {
+        {-4*M, 4*M},{-3*M, 4*M},{-2*M, 4*M},{-1*M, 4*M},{0*M, 4*M},{1*M, 4*M},{2*M, 4*M},{3*M, 4*M},{4*M, 4*M},
+        {-4*M, 3*M},{-3*M, 3*M},{-2*M, 3*M},{-1*M, 3*M},{0*M, 3*M},{1*M, 3*M},{2*M, 3*M},{3*M, 3*M},{4*M, 3*M},
+        {-4*M, 2*M},{-3*M, 2*M},{-2*M, 2*M},{-1*M, 2*M},{0*M, 2*M},{1*M, 2*M},{2*M, 2*M},{3*M, 2*M},{4*M, 2*M},
+        {-4*M, 1*M},{-3*M, 1*M},{-2*M, 1*M},{-1*M, 1*M},{0*M, 1*M},{1*M, 1*M},{2*M, 1*M},{3*M, 1*M},{4*M, 1*M},
+        {-4*M, 0*M},{-3*M, 0*M},{-2*M, 0*M},{-1*M, 0*M},{0*M, 0*M},{1*M, 0*M},{2*M, 0*M},{3*M, 0*M},{4*M, 0*M},
+        {-4*M,-1*M},{-3*M,-1*M},{-2*M,-1*M},{-1*M,-1*M},{0*M,-1*M},{1*M,-1*M},{2*M,-1*M},{3*M,-1*M},{4*M,-1*M},
+        {-4*M,-2*M},{-3*M,-2*M},{-2*M,-2*M},{-1*M,-2*M},{0*M,-2*M},{1*M,-2*M},{2*M,-2*M},{3*M,-2*M},{4*M,-2*M},
+        {-4*M,-3*M},{-3*M,-3*M},{-2*M,-3*M},{-1*M,-3*M},{0*M,-3*M},{1*M,-3*M},{2*M,-3*M},{3*M,-3*M},{4*M,-3*M},
+        {-4*M,-4*M},{-3*M,-4*M},{-2*M,-4*M},{-1*M,-4*M},{0*M,-4*M},{1*M,-4*M},{2*M,-4*M},{3*M,-4*M},{4*M,-4*M},
+    };
+
+    public void Render(GameObject board, GameObject piece, int col, int row, int type) {
+        int idx = (row - 1) * 9 + (9 - col);
+        float x = POSITIONS[idx, COL];
+        float y = POSITIONS[idx, ROW];
         piece.transform.SetParent(board.transform, false);
         piece.transform.localPosition = new Vector3(
-            0f,
-            0f,
+            x,
+            y,
             0f);
+        Debug.Log(idx);
+        Debug.Log(x);
+        Debug.Log(y);
 
         piece.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(PIECE_IMG_DIR + piece_names[type]);
     }
