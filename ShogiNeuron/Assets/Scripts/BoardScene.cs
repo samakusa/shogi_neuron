2cb8ab81beac908f74717b95669e41987166510e
samakusa <sirouamakusa0412@gmail.com>
2020-12-30 12:44:32 +0900

    Implemented piece drawing process

--- ShogiNeuron/Assets/Scripts/BoardScene.cs
+++ ShogiNeuron/Assets/Scripts/BoardScene.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardScene : MonoBehaviour {
-    public GameObject piecePrefab;
     public GameObject onBoard;
+    public GameObject piecePrefab;
+
+    private PieceRenderer PieceRenderer;
+    private string begin_board_sfen = "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b -";
+
+    private const float M = 0.3525f;
+    private const int COL = 0;  // index
+    private const int ROW = 1;  // index
+    private float[,] p = {
+        {-4*M,-4*M},{-3*M,-4*M},{-2*M,-4*M},{-1*M,-4*M},{0*M,-4*M},{1*M,-4*M},{2*M,-4*M},{3*M,-4*M},{4*M,-4*M},
+        {-4*M,-3*M},{-3*M,-3*M},{-2*M,-3*M},{-1*M,-3*M},{0*M,-3*M},{1*M,-3*M},{2*M,-3*M},{3*M,-3*M},{4*M,-3*M},
+        {-4*M,-2*M},{-3*M,-2*M},{-2*M,-2*M},{-1*M,-2*M},{0*M,-2*M},{1*M,-2*M},{2*M,-2*M},{3*M,-2*M},{4*M,-2*M},
+        {-4*M,-1*M},{-3*M,-1*M},{-2*M,-1*M},{-1*M,-1*M},{0*M,-1*M},{1*M,-1*M},{2*M,-1*M},{3*M,-1*M},{4*M,-1*M},
+        {-4*M, 0*M},{-3*M, 0*M},{-2*M, 0*M},{-1*M, 0*M},{0*M, 0*M},{1*M, 0*M},{2*M, 0*M},{3*M, 0*M},{4*M, 0*M},
+        {-4*M, 1*M},{-3*M, 1*M},{-2*M, 1*M},{-1*M, 1*M},{0*M, 1*M},{1*M, 1*M},{2*M, 1*M},{3*M, 1*M},{4*M, 1*M},
+        {-4*M, 2*M},{-3*M, 2*M},{-2*M, 2*M},{-1*M, 2*M},{0*M, 2*M},{1*M, 2*M},{2*M, 2*M},{3*M, 2*M},{4*M, 2*M},
+        {-4*M, 3*M},{-3*M, 3*M},{-2*M, 3*M},{-1*M, 3*M},{0*M, 3*M},{1*M, 3*M},{2*M, 3*M},{3*M, 3*M},{4*M, 3*M},
+        {-4*M, 4*M},{-3*M, 4*M},{-2*M, 4*M},{-1*M, 4*M},{0*M, 4*M},{1*M, 4*M},{2*M, 4*M},{3*M, 4*M},{4*M, 4*M},
+    };
+
+    private enum piece_types {
+        NONE, PORN, LANCE, KNIGHT, SILVER, BISHOP, ROOK, GOLD, KING,
+        PORN_PROMOTED, LANCE_PROMOTED, KNIGHT_PROMOTED, SILVER_PROMOTED, BISHOP_PROMOTED, ROOK_PROMOTED,
+    };
 
     // Start is called before the first frame update
     void Start() {
-        GameObject piece = (GameObject)Instantiate(piecePrefab);
-        piece.transform.SetParent(this.onBoard.transform, false);
-        piece.transform.localPosition = new Vector3(
-            0f,
-            0f,
-            0f);
+        this.PieceRenderer = new PieceRenderer();
+        this.PieceRenderer.Render(this.onBoard, this.piecePrefab, 0, 0, (int)piece_types.LANCE);
     }
 
     // Update is called once per frame
---
+++ ShogiNeuron/Assets/Scripts/PieceRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceRenderer : MonoBehaviour
+{
+    private const string PIECE_IMG_DIR = "Images/";
+    private string[] piece_names = {
+        "",
+        "porn",
+        "lance",
+        "knight",
+        "silver",
+        "bishop",
+        "rook",
+        "porn_promoted",
+        "lance_promoted",
+        "knight_promoted",
+        "silver_promoted",
+        "bishop_promoted",
+        "rook_promoted",
+        "gold",
+        "king",
+    };
+
+    public void Render(GameObject board, GameObject basePiece, int col, int row, int type) {
+        GameObject piece = (GameObject)Instantiate(basePiece);
+        piece.transform.SetParent(board.transform, false);
+        piece.transform.localPosition = new Vector3(
+            0f,
+            0f,
+            0f);
+
+        piece.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(PIECE_IMG_DIR + piece_names[type]);
+    }
+
+    public void RenderSfen() {
+    }
+}
