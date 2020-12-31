6a99299ef55c86191d60034c1f4f99c6a0f2deef
samakusa <sirouamakusa0412@gmail.com>
2020-12-31 11:00:17 +0900

    Implemented piece drawing process with SFEN character string input
    only on the board.

--- ShogiNeuron/Assets/Scripts/PieceRenderer.cs
+++ ShogiNeuron/Assets/Scripts/PieceRenderer.cs
@@ -1,6 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PieceRenderer {
+    public enum piece_types {
+        NONE, PORN, LANCE, KNIGHT, SILVER, BISHOP, ROOK, GOLD, KING,
+        PORN_PROMOTED, LANCE_PROMOTED, KNIGHT_PROMOTED, SILVER_PROMOTED, BISHOP_PROMOTED, ROOK_PROMOTED,
+    };
+
     private const string PIECE_IMG_DIR = "Images/";
     private string[] piece_names = {
         "",
@@ -20,7 +27,12 @@ public class PieceRenderer {
         "rook_promoted",
     };
 
-    private const float M = 0.39f;
+    public enum turn {
+        BLACK,
+        WHITE,
+    }
+
+    private const float M = 0.39f;  // margin
     private const int COL = 0;  // index
     private const int ROW = 1;  // index
     private float[,] POSITIONS = {
@@ -35,8 +47,13 @@ public class PieceRenderer {
         {-4*M,-4*M},{-3*M,-4*M},{-2*M,-4*M},{-1*M,-4*M},{0*M,-4*M},{1*M,-4*M},{2*M,-4*M},{3*M,-4*M},{4*M,-4*M},
     };
 
-    public void Render(GameObject board, GameObject piece, int col, int row, int type, bool is_white=false) {
+    public void Render(GameObject board, GameObject piecePrefab, int col, int row, piece_types type, turn turn) {
         int idx = (row - 1) * 9 + (9 - col);
+        Render(board, piecePrefab, idx, type, turn);
+    }
+
+    public void Render(GameObject board, GameObject piecePrefab, int idx, piece_types type, turn turn) {
+        GameObject piece = Object.Instantiate(piecePrefab);
         float x = POSITIONS[idx, COL];
         float y = POSITIONS[idx, ROW];
         piece.transform.SetParent(board.transform, false);
@@ -44,15 +61,158 @@ public class PieceRenderer {
             x,
             y,
             0f);
-        Debug.Log(idx);
-        Debug.Log(x);
-        Debug.Log(y);
 
-        piece.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(PIECE_IMG_DIR + piece_names[type]);
-        if (is_white)
+        piece.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(PIECE_IMG_DIR + piece_names[(int)type]);
+        if (turn == turn.WHITE)
             piece.transform.Rotate(new Vector3(180.0f, 180.0f, 0.0f));
     }
 
-    public void RenderSfen() {
+    public void RenderSfen(GameObject board, GameObject piecePrefab, string sfen, List<GameObject> pieces) {
+        int idx = 0;
+        string p = "";
+        foreach (var c in sfen) {
+            turn t;
+            piece_types type = piece_types.NONE;
+            int i;
+            p += c;
+
+            Debug.Log(p);
+            if (int.TryParse(p, out i)) {
+                idx += i;
+                p = "";
+                continue;
+            }
+            else if (p == "/") {
+                // Assert(idx % 9 == 8)
+                p = "";
+                continue;
+            }
+            else if (p == "+") {
+                continue;
+            }
+            else if (p == " ") {
+                break;
+            }
+            else if (p == "P") {
+                type = piece_types.PORN;
+                t = turn.BLACK;
+            }
+            else if (p == "L") {
+                type = piece_types.LANCE;
+                t = turn.BLACK;
+            }
+            else if (p == "N") {
+                type = piece_types.KNIGHT;
+                t = turn.BLACK;
+            }
+            else if (p == "S") {
+                type = piece_types.SILVER;
+                t = turn.BLACK;
+            }
+            else if (p == "B") {
+                type = piece_types.BISHOP;
+                t = turn.BLACK;
+            }
+            else if (p == "R") {
+                type = piece_types.ROOK;
+                t = turn.BLACK;
+            }
+            else if (p == "G") {
+                type = piece_types.GOLD;
+                t = turn.BLACK;
+            }
+            else if (p == "K") {
+                type = piece_types.KING;
+                t = turn.BLACK;
+            }
+            else if (p == "+P") {
+                type = piece_types.PORN_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "+L") {
+                type = piece_types.LANCE_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "+N") {
+                type = piece_types.KNIGHT_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "+S") {
+                type = piece_types.SILVER_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "+B") {
+                type = piece_types.BISHOP_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "+R") {
+                type = piece_types.ROOK_PROMOTED;
+                t = turn.BLACK;
+            }
+            else if (p == "p") {
+                type = piece_types.PORN;
+                t = turn.WHITE;
+            }
+            else if (p == "l") {
+                type = piece_types.LANCE;
+                t = turn.WHITE;
+            }
+            else if (p == "n") {
+                type = piece_types.KNIGHT;
+                t = turn.WHITE;
+            }
+            else if (p == "s") {
+                type = piece_types.SILVER;
+                t = turn.WHITE;
+            }
+            else if (p == "b") {
+                type = piece_types.BISHOP;
+                t = turn.WHITE;
+            }
+            else if (p == "r") {
+                type = piece_types.ROOK;
+                t = turn.WHITE;
+            }
+            else if (p == "g") {
+                type = piece_types.GOLD;
+                t = turn.WHITE;
+            }
+            else if (p == "k") {
+                type = piece_types.KING;
+                t = turn.WHITE;
+            }
+            else if (p == "+p") {
+                type = piece_types.PORN_PROMOTED;
+                t = turn.WHITE;
+            }
+            else if (p == "+l") {
+                type = piece_types.LANCE_PROMOTED;
+                t = turn.WHITE;
+            }
+            else if (p == "+n") {
+                type = piece_types.KNIGHT_PROMOTED;
+                t = turn.WHITE;
+            }
+            else if (p == "+s") {
+                type = piece_types.SILVER_PROMOTED;
+                t = turn.WHITE;
+            }
+            else if (p == "+b") {
+                type = piece_types.BISHOP_PROMOTED;
+                t = turn.WHITE;
+            }
+            else if (p == "+r") {
+                type = piece_types.ROOK_PROMOTED;
+                t = turn.WHITE;
+            }
+            else {
+                // Assert(true);
+                p = "";
+                continue;
+            }
+
+            Render(board, piecePrefab, idx, type, t);
+            p = ""; idx++;
+        }
     }
 }
